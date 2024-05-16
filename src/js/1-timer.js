bc765bbb97394aed8313a82a1a362ee50bc09eed
allaponomarenko <alla.deyneka03@gmail.com>
2024-05-16 10:52:41 +0200

    2-snackbar.css_listo

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,5 @@
+
+
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,28 +1,62 @@
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-
-//-------------------------------------------------------------------
 document.addEventListener('DOMContentLoaded', () => {
-    // Додати клас до input для затримки
-    const delayInput = document.querySelector('input[name="delay"]');
-    if (delayInput) {
-      delayInput.classList.add('delay-input');
-    }
-  
-    // Додати клас до radio input елементів
-    const stateInputs = document.querySelectorAll('input[name="state"]');
-    stateInputs.forEach((input) => {
-      if (input.value === 'fulfilled') {
-        input.classList.add('fulfilled-input');
-      } else if (input.value === 'rejected') {
-        input.classList.add('rejected-input');
-      }
-    });
-  
-    // Додати клас до fieldset
-    const fieldset = document.querySelector('fieldset');
-    if (fieldset) {
-      fieldset.classList.add('state-fieldset');
+  const delayInput = document.querySelector('input[name="delay"]');
+  if (delayInput) {
+    delayInput.classList.add('delay-input');
+  }
+
+ 
+  const stateInputs = document.querySelectorAll('input[name="state"]');
+  stateInputs.forEach((input) => {
+    if (input.value === 'fulfilled') {
+      input.classList.add('fulfilled-input');
+    } else if (input.value === 'rejected') {
+      input.classList.add('rejected-input');
     }
   });
-  
+
+
+  const fieldset = document.querySelector('fieldset');
+  if (fieldset) {
+    fieldset.classList.add('state-fieldset');
+  }
+
+  const form = document.querySelector('.form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const delay = parseInt(delayInput.value);
+    const selectedState = Array.from(stateInputs).find(input => input.checked).value;
+
+    createPromise(delay, selectedState)
+      .then((delay) => {
+        iziToast.success({
+          title: 'Success',
+          message: `✅ Fulfilled promise in ${delay}ms`,
+          position: 'topCenter'
+        });
+      })
+      .catch((delay) => {
+        iziToast.error({
+          title: 'Error',
+          message: `❌ Rejected promise in ${delay}ms`,
+          position: 'topCenter'
+        });
+      });
+  });
+
+  function createPromise(delay, state) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+  }
+});
